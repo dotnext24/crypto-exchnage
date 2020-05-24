daf067d490429e566076f12dcac42958e3a8e66a
Dinesh Kumar <dotnext24@gmail.com>
2020-05-24 21:38:13 +0530

    responsiveness

--- client/src/components/token-exchange/CurrencyDropdown.js
+++ client/src/components/token-exchange/CurrencyDropdown.js
@@ -65,9 +65,9 @@ export default class CurrencyDropdown extends Component {
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M7.22222 13.4444C10.6587 13.4444 13.4444 10.6587 13.4444 7.22222C13.4444 3.78578 10.6587 1 7.22222 1C3.78578 1 1 3.78578 1 7.22222C1 10.6587 3.78578 13.4444 7.22222 13.4444Z" stroke="#80A3B6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                                 <path d="M15.0005 14.9995L11.6172 11.6162" stroke="#80A3B6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                         </i>
-                        <input type="text" placeholder="Type a currency or ticker" onChange={this.handleChange} value={this.state.value} />
+                        <input className="form-control" type="text" placeholder="Type a currency or ticker" onChange={this.handleChange} value={this.state.value} />
                     </div>
-                    <button type="button" tabindex="-1" className="sc-chPdSV iEQJel">
+                    <button type="button" tabindex="-1" className="btn sc-chPdSV iEQJel">
                         <i onClick={this.props.onClose} style={{ "display": "inline-block", "vertical-align": "middle" }}>
                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M14 2L2 14" stroke="#80A3B6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
--- client/src/components/token-exchange/Exchange.js
+++ client/src/components/token-exchange/Exchange.js
@@ -329,7 +329,7 @@ export default class Exchange extends Component {
 
                                 <div className="currency-block styled__WrapperCurrency-g3y0ua-0 rGnYa send-box" style={{}}>
                                     <span className="currency-block__label styled__CurrencyLabel-g3y0ua-1 biCxOe">You send</span>
-                                    <input style={this.isValid()?{}:{'border-color': 'rgba(255, 176, 0, 0.294)'}} onChange={this.handleSendValueChange} value={this.state.sendValue} maxLength="16" className="currency-block__value styled__CurrencyValue-g3y0ua-2 dtUlLd" />
+                                    <input style={this.isValid()?{}:{'border-color': 'rgba(255, 176, 0, 0.294)'}} onChange={this.handleSendValueChange} value={this.state.sendValue} maxLength="16" className="form-control currency-block__value styled__CurrencyValue-g3y0ua-2 dtUlLd" />
                                     <div className="currency-block__currency styled__CurrencyButtonWrapper-g3y0ua-3 jCKRds">
                                         <button onClick={this.handleShowSendCurrency.bind()} className="currency-block__switch switchable styled__CurrencySwitch-g3y0ua-4 ZmPKt" type="button" id="currency_button_from">
                                             <div className="full-name-label">{this.state.sendCurrency && this.state.sendCurrency.NAME ? this.state.sendCurrency.NAME : "US Dollar"}</div>{this.state.sendCurrency && this.state.sendCurrency.SYMBOL ? this.state.sendCurrency.SYMBOL : "usd"}
